8f064a756b58a55e43d4d4311447afe0bf7eadba
Leonardo <leocompa10@gmail.com>
2024-02-01 18:00:54 +0100

    IntelliJ: - refactoring - remove TODO - Inizializzazione con Spring Boot - DataBase

--- src/main/java/ids/unicam/DataBase/DatabaseManager.java
+++ src/main/java/ids/unicam/DataBase/DatabaseManager.java
@@ -1,63 +1,72 @@
 package ids.unicam.DataBase;
+import ids.unicam.controller.UtentiController;
+import ids.unicam.models.attori.GestorePiattaforma;
+import ids.unicam.models.attori.TuristaLoggato;
+
 import java.sql.*;
+import java.util.Date;
+import java.util.List;
 
 public class DatabaseManager {
 
-        // URL di connessione al database H2 (memoria)
-        private static final String DB_URL = "jdbc:h2:mem:testdb";
+    // URL di connessione al database H2 (memoria)
+    private static final String DB_URL = "jdbc:h2:./src/main/resources/DBStorage/new_file";
+    // Nome utente e password del database
+    private static final String USER = "user";
+    private static final String PASSWORD = "pass";
 
-        // Nome utente e password del database (scegliere come preferisci)
-        private static String user = "user";
-        private static String password = "pass";
+    public static void creaDB() {
+        try {
+            UtentiController utentiController = new UtentiController();
+            GestorePiattaforma gestorePiattaforma = new GestorePiattaforma();
+            gestorePiattaforma.getGestoreController().registraTurista("Leonardo","Compagnucci",new Date(), "UNICAM", "leocompa");
+            // Carica il driver JDBC per H2
+            Class.forName("org.h2.Driver");
 
-        public static void creaDB() {
-            try {
-                // Carica il driver JDBC per H2
-                Class.forName("org.h2.Driver");
-
-                // Crea la connessione al database
-                Connection connection = DriverManager.getConnection(DB_URL, user, password);
-
+            // Crea la connessione al database
+            try (Connection connection = DriverManager.getConnection(DB_URL, USER, PASSWORD)) {
                 // Esegui le operazioni desiderate sul database
                 // Esempio: crea una tabella
                 String createTableSQL =
-                        "CREATE TABLE CIAO (" +
-                        "id INT PRIMARY KEY AUTO_INCREMENT," +
-                        "username VARCHAR(50) NOT NULL," +
-                        "password VARCHAR(50) NOT NULL)";
+                        "CREATE TABLE IF NOT EXISTS TURISTI(" +
+                                "id VARCHAR(50) PRIMARY KEY," +
+                                "name VARCHAR(50) NOT NULL," +
+                                "surname VARCHAR(50) NOT NULL," +
+                                "username VARCHAR(50) NOT NULL," +
+                                "password VARCHAR(50) NOT NULL)";
                 connection.createStatement().executeUpdate(createTableSQL);
 
                 // Esempio: inserisci dei dati nella tabella
-                String insertDataSQL = "INSERT INTO CIAO (username, password) VALUES ('user1', 'password1')";
-                //connection.createStatement().executeUpdate(insertDataSQL);
+                String insertDataSQL = "INSERT INTO TURISTI (id, name, surname, username, password) VALUES (?, ?, ?, ?, ?)";
+                PreparedStatement statement = connection.prepareStatement(insertDataSQL);
+                List<TuristaLoggato> turisti = utentiController.getTuristi();
 
-                Statement statement = connection.createStatement();
-                int rowsAffected = statement.executeUpdate(insertDataSQL);
-                System.out.println("Numero di righe inserite: " + rowsAffected);
+                for (TuristaLoggato turistaLoggato : turisti) {
+                    statement.setString(1, turistaLoggato.getId());
+                    statement.setString(2, turistaLoggato.getName());
+                    statement.setString(3, turistaLoggato.getSurname());
+                    statement.setString(4, turistaLoggato.getUsername());
+                    statement.setString(5, turistaLoggato.getPassword());
+                    statement.executeUpdate();
+                }
 
                 // Esegui una query di selezione per ottenere i dati appena inseriti
-                String selectSQL = "SELECT * FROM CIAO WHERE username = 'user1'";
+                String selectSQL = "SELECT * FROM TURISTI WHERE username = 'Leonardo'";
                 ResultSet resultSet = statement.executeQuery(selectSQL);
 
                 // Stampa i risultati della query
                 while (resultSet.next()) {
-                    int id = resultSet.getInt("id");
+                    String id = resultSet.getString("id");
                     String username = resultSet.getString("username");
                     String password = resultSet.getString("password");
                     System.out.println("ID: " + id + ", Username: " + username + ", Password: " + password);
                 }
-
-
-
-                // Chiudi la connessione al database
-                resultSet.close();
-                statement.close();
-                connection.commit();
-                connection.close();
-
-                System.out.println("Operazioni sul database H2 completate con successo.");
-            } catch (ClassNotFoundException | SQLException e) {
-                e.printStackTrace();
             }
+
+            System.out.println("Operazioni sul database H2 completate con successo.");
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
         }
+    }
 }
+
