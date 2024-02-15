570173e7ed81194adaf1125f8ccf22aa79df17b1
umber <umbertodiantonio@gmail.com>
2024-02-15 14:55:04 +0100

    intelliJ: - fixes

--- src/main/java/ids/unicam/models/attori/Turista.java
+++ src/main/java/ids/unicam/models/attori/Turista.java
@@ -6,16 +6,5 @@ import java.util.ArrayList;
 
 
 public class Turista {
-    public void report(PuntoInteresse puntoInteresse){
-        throw new UnsupportedOperationException();
-        //TODO segnala un contenuto al Curatore
-    }
 
-    /**
-     * Ti fa accedere con a uno dei tuoi account, che varia in base al comune selezionato
-     */
-    public void accedi(){
-        throw new UnsupportedOperationException();
-        //TODO
-    }
 }
