93b23f09f9d756d5f1a85139389db5dbda4ffb51
Leonardo <leocompa10@gmail.com>
2024-02-23 12:30:00 +0100

    IntelliJ:

--- src/main/java/ids/unicam/Main.java
+++ src/main/java/ids/unicam/Main.java
@@ -4,7 +4,6 @@ package ids.unicam;
 import ids.unicam.DataBase.GestoreDatabase;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
