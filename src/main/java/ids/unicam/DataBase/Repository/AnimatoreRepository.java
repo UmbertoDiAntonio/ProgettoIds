c0a9553e2c877a2d9cb3143a92c4da86704b2841
umber <umbertodiantonio@gmail.com>
2024-02-23 16:02:29 +0100

    intelliJ: - Fix

--- src/main/java/ids/unicam/DataBase/Repository/AnimatoreRepository.java
+++ src/main/java/ids/unicam/DataBase/Repository/AnimatoreRepository.java
@@ -3,15 +3,18 @@ package ids.unicam.DataBase.Repository;
 import ids.unicam.models.Comune;
 import ids.unicam.models.attori.Animatore;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface AnimatoreRepository extends JpaRepository<Animatore, String> {
     List<Animatore> findByCognome(String cognome);
 
-    List<Animatore> findByNome(String nome);
+    @Query("select a from Animatore a where a.username=:username")
+    Optional<Animatore> findByUsername(String username);
 
     List<Animatore> findByComune(Comune comune);
 
